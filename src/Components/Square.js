95ddfbf911c34971aa697ac2d9b65dd44480cfef
WILLIAM STEINBRINK <william.steinbrink@gmail.com>
2022-01-07 15:23:03 -0500

    third

--- src/Components/Square.js
+++ src/Components/Square.js
@@ -4,13 +4,18 @@ const Square = ({ id, player }) => {
     const [color, setColor] = React.useState('green');
     const palet = ['red', 'blue', 'green'];
     const getRandomColor = () => palet[Math.floor(Math.random() * 3)]
+    React.useEffect(() => {
+        console.log(`Render ${id}`);
+        return () => console.log(`Unmounting Square ${id}`)
+    })
 
     return (
         <button onClick={(e) => {
-            alert(`I'm Square ${id}`)
             setColor(getRandomColor());
             e.target.style.background = color;
-        }}> <h1>{id}</h1> </button>
+        }}>
+            <h1>{id}</h1>
+        </button>
     );
 };
 
