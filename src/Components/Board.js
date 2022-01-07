b4dd8bf7209fd1bbc62e3760f1187148ede5d059
WilliamSteinbrink <william.steinbrink@gmail.com>
2022-01-07 16:32:01 -0500

    Added newState

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -3,32 +3,28 @@ import Square from './Square';
 
 const Board = () => {
     const [player, setPlayer] = React.useState(1);
-    const [mounted, setMounted] = React.useState(true);
-    const [random, setRandom] = React.useState(0);
-    let [status, setStatus] = React.useState(`Player 1`);
+    const [state, setState] = React.useState([]);
 
-    const toggle = () => setMounted(!mounted);
+    let status = `Player ${player}`;
 
-    const reRender = () => setRandom(Math.random());
+    const newState = (obj) => {
+        setState([...state, obj]);
+        console.log(`adding state ${JSON.stringify(state)}`);
+    }
 
     function renderSquare(i) {
-        return <Square id={i} player={player}></Square>;
+        return <Square id={i} player={player} newState={newState}></Square>;
     }
     return (
-        <div className="game-board"
-            onClick={(e) => {
-                (player === 1) ? setPlayer(2) : setPlayer(1);
-                setStatus(`Player ${player}`);
-            }}
-        >
+        <div className="game-board">
             <div className="grid-row">
-                {mounted && renderSquare(0)}
-                {mounted && renderSquare(1)}
-                {mounted && renderSquare(2)}
+                {renderSquare(0)}
+                {renderSquare(1)}
+                {renderSquare(2)}
             </div>
             <div id="info">
-                <button onClick={toggle}>Show/Hide Row</button>
-                <button onClick={reRender}>Re-render</button>
+                <button>Show/Hide Row</button>
+                <button>Re-render</button>
                 <h1>{status}</h1>
             </div>
         </div>
--- src/Components/Square.js
+++ src/Components/Square.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Square = ({ id, player }) => {
+const Square = ({ id, player, newState }) => {
     const [color, setColor] = React.useState('green');
     const palet = ['red', 'blue', 'green'];
     const getRandomColor = () => palet[Math.floor(Math.random() * 3)]
@@ -11,8 +11,10 @@ const Square = ({ id, player }) => {
 
     return (
         <button onClick={(e) => {
-            setColor(getRandomColor());
-            e.target.style.background = color;
+            let col = getRandomColor();
+            setColor(col);
+            newState({id:id, color:color});
+            e.target.style.background = col;
         }}>
             <h1>{id}</h1>
         </button>
